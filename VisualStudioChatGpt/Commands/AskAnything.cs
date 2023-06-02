b46e29d8989ca4f6fa6283cba90e8fe83e51bbb3
WangShuYu <674613047@qq.com>
2023-06-02 09:56:43 +0800

    从github迁移到gitee中 github太卡了

---
+++ VisualStudioChatGpt/Commands/AskAnything.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VisualStudioChatGpt.Model;
+
+namespace VisualStudioChatGpt.Commands
+{
+    /// <summary>
+    /// 对话提问
+    /// </summary>
+    internal class AskAnything : MyBase
+    {
+        internal override void VirHandler(object sender, EventArgs e)
+        {
+            Process.Start("https://chat.openai.com/");
+            //base.VirHandler(sender, e);
+        }
+    }
+}
